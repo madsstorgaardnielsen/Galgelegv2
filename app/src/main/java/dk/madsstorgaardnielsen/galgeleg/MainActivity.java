9da7e979e4c504d527ce74c99181aaced26e4271
Mads Storgaard-Nielsen <s180076@win.dtu.dk>
2020-09-29 13:24:03 +0200

    added todo

--- app/src/main/java/dk/madsstorgaardnielsen/galgeleg/MainActivity.java
+++ app/src/main/java/dk/madsstorgaardnielsen/galgeleg/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//TODO STARTMENU, HIGHSCORES, BEDRE LOGIK, AFSLUT SPIL SKAL TAGE EN TIL MENU OG IKKE LUKKE APP
 
     Button startNewGame;
     Intent intent;
